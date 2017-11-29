12b7536227e50ac1522a4c3e128d736ef3bb0c55
hungteshun <wangyiyx201495@163.com>
2017-11-29 22:44:49 +0800

    优化部署流程定义

--- activiti_leave/src/main/java/com/hungteshun/HelloActiviti.java
+++ activiti_leave/src/main/java/com/hungteshun/HelloActiviti.java
@@ -5,6 +5,12 @@ import org.activiti.engine.ProcessEngines;
 import org.activiti.engine.repository.Deployment;
 import org.junit.Test;
 
+/**
+ * 运行activiti测试
+ * @author hungteshun
+ *
+ */
+
 public class HelloActiviti {
 
 	ProcessEngine processEngine = ProcessEngines.getDefaultProcessEngine();
@@ -12,8 +18,12 @@ public class HelloActiviti {
 	@Test
 	public void deploymentProcessDefinition(){
 		Deployment deployment = processEngine.getRepositoryService()//与流程定义和部署对象相关的Service
-						.createDeployment().name("部署helloActiviti工程").addClasspathResource("diagrams/helloActiviti.bpmn").addClasspathResource("diagrams/helloActiviti.png").deploy();
+						.createDeployment()//创建一个部署对象
+						.name("部署helloActiviti工程")//添加部署的名称
+						.addClasspathResource("diagrams/helloActiviti.bpmn")//从classpath的资源中加载，一次只能加载一个文件
+						.addClasspathResource("diagrams/helloActiviti.png")//从classpath的资源中加载，一次只能加载一个文件
+						.deploy();//创建部署对象
 		System.out.println("部署ID："+deployment.getId());//1
-		System.out.println("部署名称："+deployment.getName());//
+		System.out.println("部署名称："+deployment.getName());//部署helloActiviti工程
 	}
 }

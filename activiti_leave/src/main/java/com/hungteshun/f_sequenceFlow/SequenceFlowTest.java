33c1c734597c42b3b5e1462ed5cb2bfa425bb391
hungteshun <wangyiyx201495@163.com>
2017-12-04 11:14:33 +0800

    连线相关操作-以inputStream方式部署流程定义优化配置buildPath

--- activiti_leave/src/main/java/com/hungteshun/f_sequenceFlow/SequenceFlowTest.java
+++ activiti_leave/src/main/java/com/hungteshun/f_sequenceFlow/SequenceFlowTest.java
@@ -9,25 +9,28 @@ import org.junit.Test;
 
 /**
  * 连线相关操作
+ * 
  * @author hungteshun
  *
  */
 public class SequenceFlowTest {
-	
+
 	ProcessEngine processEngine = ProcessEngines.getDefaultProcessEngine();
-	
-	/**部署流程定义（从inputStream）*/
+
+	/** 部署流程定义（从inputStream） */
 	@Test
-	public void deploymentProcessDefinition_inputStream(){
+	public void deploymentProcessDefinition_inputStream() {
+		// 注意这里的bpmn文件的路径，不是放在resource目录下的，所以要修改buildPath信息
 		InputStream inputStreamBpmn = this.getClass().getResourceAsStream("sequenceFlow.bpmn");
+		// 注意这里的png文件的路径，不是放在resource目录下的，所以要修改buildPath信息
 		InputStream inputStreamPng = this.getClass().getResourceAsStream("sequenceFlow.png");
-		Deployment deployment = processEngine.getRepositoryService()//与流程定义和部署对象相关的Service
-						.createDeployment()//创建一个部署对象
-						.name("连线")//添加部署的名称
-						.addInputStream("sequenceFlow.bpmn", inputStreamBpmn)//
-						.addInputStream("sequenceFlow.png", inputStreamPng)//
-						.deploy();//完成部署
-		System.out.println("部署ID："+deployment.getId());//
-		System.out.println("部署名称："+deployment.getName());//
+		Deployment deployment = processEngine.getRepositoryService()// 与流程定义和部署对象相关的Service
+				.createDeployment()// 创建一个部署对象
+				.name("连线")// 添加部署的名称
+				.addInputStream("sequenceFlow.bpmn", inputStreamBpmn)//
+				.addInputStream("sequenceFlow.png", inputStreamPng)//
+				.deploy();// 完成部署
+		System.out.println("部署ID：" + deployment.getId());//
+		System.out.println("部署名称：" + deployment.getName());//
 	}
 }

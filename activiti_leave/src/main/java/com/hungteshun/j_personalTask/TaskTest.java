882a09e7009f71ffdb9b2e9281abfe1f2564c807
hungteshun <wangyiyx201495@163.com>
2017-12-07 10:36:16 +0800

    启动流程实例--查询当前人的个人任务

--- activiti_leave/src/main/java/com/hungteshun/j_personalTask/TaskTest.java
+++ activiti_leave/src/main/java/com/hungteshun/j_personalTask/TaskTest.java
@@ -2,12 +2,14 @@ package com.hungteshun.j_personalTask;
 
 import java.io.InputStream;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 import org.activiti.engine.ProcessEngine;
 import org.activiti.engine.ProcessEngines;
 import org.activiti.engine.repository.Deployment;
 import org.activiti.engine.runtime.ProcessInstance;
+import org.activiti.engine.task.Task;
 import org.junit.Test;
 
 /**
@@ -53,10 +55,43 @@ public class TaskTest {
 		String processDefinitionKey = "task";
 		/** 启动流程实例的同时，设置流程变量，使用流程变量用来指定任务的办理人，对应task.pbmn文件中#{userID} */
 		Map<String, Object> variables = new HashMap<String, Object>();
-		variables.put("userID", "周芷若");
+		variables.put("userID", "刘德华");
 		ProcessInstance pi = processEngine.getRuntimeService()// 与正在执行的流程实例和执行对象相关的Service
 				.startProcessInstanceByKey(processDefinitionKey, variables);// 使用流程定义的key启动流程实例，key对应helloworld.bpmn文件中id的属性值，使用key值启动，默认是按照最新版本的流程定义启动
 		System.out.println("流程实例ID:" + pi.getId());// 流程实例ID
 		System.out.println("流程定义ID:" + pi.getProcessDefinitionId());// 流程定义ID
+	}
+
+	/** 查询当前人的个人任务 */
+	@Test
+	public void findMyPersonalTask() {
+		String assignee = "刘德华";
+		List<Task> list = processEngine.getTaskService()// 与正在执行的任务管理相关的Service
+				.createTaskQuery()// 创建任务查询对象
+				/** 查询条件（where部分） */
+				.taskAssignee(assignee)// 指定个人任务查询，指定办理人
+				// .taskCandidateUser(candidateUser)//组任务的办理人查询
+				// .processDefinitionId(processDefinitionId)//使用流程定义ID查询
+				// .processInstanceId(processInstanceId)//使用流程实例ID查询
+				// .executionId(executionId)//使用执行对象ID查询
+				/** 排序 */
+				.orderByTaskCreateTime().asc()// 使用创建时间的升序排列
+				/** 返回结果集 */
+				// .singleResult()//返回惟一结果集
+				// .count()//返回结果集的数量
+				// .listPage(firstResult, maxResults);//分页查询
+				.list();// 返回列表
+		if (list != null && list.size() > 0) {
+			for (Task task : list) {
+				System.out.println("任务ID:" + task.getId());
+				System.out.println("任务名称:" + task.getName());
+				System.out.println("任务的创建时间:" + task.getCreateTime());
+				System.out.println("任务的办理人:" + task.getAssignee());
+				System.out.println("流程实例ID：" + task.getProcessInstanceId());
+				System.out.println("执行对象ID:" + task.getExecutionId());
+				System.out.println("流程定义ID:" + task.getProcessDefinitionId());
+				System.out.println("########################################################");
+			}
+		}
 	}
 }

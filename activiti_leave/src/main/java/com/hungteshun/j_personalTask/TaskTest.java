4d6fd8070e3ea065745e7e7deca2f4da8ccfdcd7
hungteshun <wangyiyx201495@163.com>
2017-12-07 10:27:47 +0800

    个人任务相关操作-启动流程实例

--- activiti_leave/src/main/java/com/hungteshun/j_personalTask/TaskTest.java
+++ activiti_leave/src/main/java/com/hungteshun/j_personalTask/TaskTest.java
@@ -5,6 +5,7 @@ import java.io.InputStream;
 import org.activiti.engine.ProcessEngine;
 import org.activiti.engine.ProcessEngines;
 import org.activiti.engine.repository.Deployment;
+import org.activiti.engine.runtime.ProcessInstance;
 import org.junit.Test;
 
 /**
@@ -25,10 +26,19 @@ public class TaskTest {
 		Deployment deployment = processEngine.getRepositoryService()// 与流程定义和部署对象相关的Service
 				.createDeployment()// 创建一个部署对象
 				.name("个人任务")// 添加部署的名称
-				.addInputStream("task.bpmn", inputStreamBpmn)
-				.addInputStream("task.png", inputStreamPng)
-				.deploy();// 完成部署
+				.addInputStream("task.bpmn", inputStreamBpmn).addInputStream("task.png", inputStreamPng).deploy();// 完成部署
 		System.out.println("部署ID：" + deployment.getId());
 		System.out.println("部署名称：" + deployment.getName());
+	}
+
+	/** 启动流程实例 */
+	@Test
+	public void startProcessInstance() {
+		// 流程定义的key
+		String processDefinitionKey = "task";
+		ProcessInstance pi = processEngine.getRuntimeService()// 与正在执行的流程实例和执行对象相关的Service
+				.startProcessInstanceByKey(processDefinitionKey);// 使用流程定义的key启动流程实例，key对应helloworld.bpmn文件中id的属性值，使用key值启动，默认是按照最新版本的流程定义启动
+		System.out.println("流程实例ID:" + pi.getId());// 流程实例ID
+		System.out.println("流程定义ID:" + pi.getProcessDefinitionId());// 流程定义ID
 	}
 }

5032430f2a1a2109ffb6c14db747f848dbd1e237
hungteshun <wangyiyx201495@163.com>
2017-12-10 11:42:22 +0800

    组任务相关操作-查询当前人的组任务

--- activiti_leave/src/main/java/com/hungteshun/k_group/TaskTest.java
+++ activiti_leave/src/main/java/com/hungteshun/k_group/TaskTest.java
@@ -67,5 +67,29 @@ public class TaskTest {
 		System.out.println("流程定义ID:"+pi.getProcessDefinitionId());//流程定义ID   
 	}
 	
-	
+	/**查询当前人的组任务*/
+	@Test
+	public void findMyGroupTask(){
+		String candidateUser = "大大";
+		List<Task> list = processEngine.getTaskService()//与正在执行的任务管理相关的Service
+						.createTaskQuery()//创建任务查询对象
+						/**查询条件（where部分）*/
+						.taskCandidateUser(candidateUser)//组任务的办理人查询
+						/**排序*/
+						.orderByTaskCreateTime().asc()//使用创建时间的升序排列
+						/**返回结果集*/
+						.list();//返回列表
+		if(list!=null && list.size()>0){
+			for(Task task:list){
+				System.out.println("任务ID:"+task.getId());
+				System.out.println("任务名称:"+task.getName());
+				System.out.println("任务的创建时间:"+task.getCreateTime());
+				System.out.println("任务的办理人:"+task.getAssignee());
+				System.out.println("流程实例ID："+task.getProcessInstanceId());
+				System.out.println("执行对象ID:"+task.getExecutionId());
+				System.out.println("流程定义ID:"+task.getProcessDefinitionId());
+				System.out.println("########################################################");
+			}
+		}
+	}
 }

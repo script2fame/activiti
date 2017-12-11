beda034bc03049d6e61db1b489b1bb7e3c6f8e0b
hungteshun <wangyiyx201495@163.com>
2017-12-11 11:33:02 +0800

    组任务操作2-使用TaskListener接口实现类指定审批人-部署流程定义

---
+++ activiti_leave/src/main/java/com/hungteshun/k_group2/TaskTest.java
@@ -0,0 +1,36 @@
+package com.hungteshun.k_group2;
+
+import java.io.InputStream;
+
+import org.activiti.engine.ProcessEngine;
+import org.activiti.engine.ProcessEngines;
+import org.activiti.engine.repository.Deployment;
+import org.junit.Test;
+
+/**
+ * 组任务操作2-使用TaskListener接口实现类指定审批人
+ * 
+ * @author hungteshun
+ *
+ */
+public class TaskTest {
+
+	ProcessEngine processEngine = ProcessEngines.getDefaultProcessEngine();
+
+	/** 部署流程定义（从inputStream） */
+	@Test
+	public void deploymentProcessDefinition_inputStream() {
+		InputStream inputStreamBpmn = this.getClass().getResourceAsStream("task.bpmn");
+		InputStream inputStreamPng = this.getClass().getResourceAsStream("task.png");
+		Deployment deployment = processEngine.getRepositoryService()// 与流程定义和部署对象相关的Service
+				.createDeployment()// 创建一个部署对象
+				.name("任务")// 添加部署的名称
+				.addInputStream("task.bpmn", inputStreamBpmn)//
+				.addInputStream("task.png", inputStreamPng)//
+				.deploy();// 完成部署
+		System.out.println("部署ID：" + deployment.getId());//
+		System.out.println("部署名称：" + deployment.getName());//
+	}
+	
+	
+}

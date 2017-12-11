7ddf56f6f6f6383650e209c36b76c06989cd47a4
hungteshun <wangyiyx201495@163.com>
2017-12-11 13:23:00 +0800

    完成我的任务

--- activiti_leave/src/main/java/com/hungteshun/k_group2/TaskTest.java
+++ activiti_leave/src/main/java/com/hungteshun/k_group2/TaskTest.java
@@ -142,4 +142,14 @@ public class TaskTest {
 			}
 		}
 	}
+
+	/** 完成我的任务 */
+	@Test
+	public void completeMyPersonalTask() {
+		// 任务ID
+		String taskId = "7204";
+		processEngine.getTaskService()// 与正在执行的任务管理相关的Service
+				.complete(taskId);
+		System.out.println("完成任务：任务ID：" + taskId);
+	}
 }

788005b8a262f2ced3149f91214b17eb49dbc7d9
hungteshun <wangyiyx201495@163.com>
2017-12-11 11:34:56 +0800

    启动流程实例

--- activiti_leave/src/main/java/com/hungteshun/k_group2/TaskTest.java
+++ activiti_leave/src/main/java/com/hungteshun/k_group2/TaskTest.java
@@ -5,6 +5,7 @@ import java.io.InputStream;
 import org.activiti.engine.ProcessEngine;
 import org.activiti.engine.ProcessEngines;
 import org.activiti.engine.repository.Deployment;
+import org.activiti.engine.runtime.ProcessInstance;
 import org.junit.Test;
 
 /**
@@ -31,6 +32,15 @@ public class TaskTest {
 		System.out.println("部署ID：" + deployment.getId());//
 		System.out.println("部署名称：" + deployment.getName());//
 	}
-	
-	
+
+	/** 启动流程实例 */
+	@Test
+	public void startProcessInstance() {
+		// 流程定义的key
+		String processDefinitionKey = "task";
+		ProcessInstance pi = processEngine.getRuntimeService()// 与正在执行的流程实例和执行对象相关的Service
+				.startProcessInstanceByKey(processDefinitionKey);// 使用流程定义的key启动流程实例，key对应helloworld.bpmn文件中id的属性值，使用key值启动，默认是按照最新版本的流程定义启动
+		System.out.println("流程实例ID:" + pi.getId());// 流程实例ID
+		System.out.println("流程定义ID:" + pi.getProcessDefinitionId());// 流程定义ID
+	}
 }

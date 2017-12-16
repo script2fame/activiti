8a1e9bfd6a717bec1e6a009a8fd1dc3714dcbab7
hungteshun <wangyiyx201495@163.com>
2017-12-16 10:44:21 +0800

    ssh框架搭建activiti请假流程

---
+++ activiti_level/src/com/hungteshun/dao/impl/EmployeeDaoImpl.java
@@ -0,0 +1,9 @@
+package com.hungteshun.dao.impl;
+
+import org.springframework.orm.hibernate3.support.HibernateDaoSupport;
+
+import com.hungteshun.dao.IEmployeeDao;
+
+public class EmployeeDaoImpl extends HibernateDaoSupport implements IEmployeeDao {
+
+}
---
+++ activiti_level/src/com/hungteshun/dao/impl/LeaveBillDaoImpl.java
@@ -0,0 +1,10 @@
+package com.hungteshun.dao.impl;
+
+
+import org.springframework.orm.hibernate3.support.HibernateDaoSupport;
+import com.hungteshun.dao.ILeaveBillDao;
+
+
+public class LeaveBillDaoImpl extends HibernateDaoSupport implements ILeaveBillDao {
+
+}
